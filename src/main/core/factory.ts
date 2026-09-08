b88cd5367422d7087f51ebc49d53d5a76fb73280
Huffer <78366424+Huffer342-WSH@users.noreply.github.com>
2026-09-09 07:36:05 +0800

    fix: improve profile switching, core readiness and DNS policy overrides (#259)

--- src/main/core/factory.ts
+++ src/main/core/factory.ts
@@ -54,6 +54,11 @@ export async function generateProfile(): Promise<void> {
 
   const profile = deepMerge(JSON.parse(JSON.stringify(currentProfile)), configToMerge)
 
+  if (controlDns && profile.dns) {
+    profile.dns['proxy-server-nameserver-policy'] =
+      configToMerge.dns?.['proxy-server-nameserver-policy'] ?? {}
+  }
+
   await cleanProfile(profile, controlDns, controlSniff)
 
   runtimeConfig = profile
